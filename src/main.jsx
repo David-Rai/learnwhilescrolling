aa8b47de2a88a60650cc4a42f748b5a21390817e
david-rai <davidkhaling123@gmail.com>
2026-02-16 20:31:19 +0545

    Feat:Posthog intregrated

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { PostHogProvider } from "@posthog/react";
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
-} as const
+} 
 
 createRoot(document.getElementById("root")).render(
   <>
