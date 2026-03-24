bd7a22afbbc6c0ecb6f53a5aa0f72618e88e820c
david-rai <davidkhaling123@gmail.com>
2026-03-24 21:39:01 +0545

    Style:Change the secondary color for better UX

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
         <main className='h-full w-full flex items-center justify-center'>
             <div className="flex items-center justify-center h-full">
                 <div className="relative">
-                    <div className="w-12 h-12 border-4 border-indigo-200 rounded-full animate-spin"></div>
+                    <div className="w-12 h-12 border-4 border-white rounded-full animate-spin"></div>
                     <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin absolute top-0 left-0"></div>
                 </div>
             </div>
