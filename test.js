16f2cfccf8ddc988e191e77167c681a5b90bc94a
david-rai <davidkhaling123@gmail.com>
2026-07-16 23:23:25 +0545

    Docs:DB schema added

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 const d = new Date();
 const date=d.toISOString().split('T')[0]
-// console.log(date.getDate() - 1);
+console.log(date)
+console.log(date)
+console.log(date)
+console.log(date)
+console.log(date)
+console.log(date)
+console.log(date)
 console.log(date)
